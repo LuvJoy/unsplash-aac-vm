56a503b27208ba805b2968c9875d2653125e1a73
zoey <zooey4u@gmail.ccom>
2020-12-26 14:39:22 +0900

    Initial_Commit
    # gradle setting
    # hide APIKEY

--- app/src/main/java/com/joseph/unsplash_mvvm/MainActivity.kt
+++ app/src/main/java/com/joseph/unsplash_mvvm/MainActivity.kt
@@ -2,10 +2,12 @@ package com.joseph.unsplash_mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.os.BuildCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
