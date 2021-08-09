fe073b74b1c0a5a9dfa075975573b8836401289d
Joseph <wjsdudtkd95@gmail.com>
2021-08-09 21:53:58 +0900

    feat: 사진 불러올때 애니메이션 추가

--- app/src/main/java/com/joseph/unsplash_mvvm/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/joseph/unsplash_mvvm/ui/detail/DetailActivity.kt
@@ -14,6 +14,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.Target
@@ -129,6 +130,7 @@ class DetailActivity : AppCompatActivity() {
             "${photo.location?.city ?: ""} ${photo.location?.country ?: ""}"
         Glide.with(this@DetailActivity)
             .load(photo.urls?.full)
+            .transition(DrawableTransitionOptions.withCrossFade())
             .apply(
                 RequestOptions()
                     .fitCenter()
