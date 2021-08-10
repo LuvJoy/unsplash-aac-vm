3b4c8558274e54dab7b6075556209d6b8715ccea
Joseph <wjsdudtkd95@gmail.com>
2021-08-10 23:42:50 +0900

    feat: SearchFragment의 기능 구현
    - 이전 검색어와 현재 검색어가 같은경우 page을 증가시켜
    사진을 더 불러오도록 함 다를경우 페이지를 1로 초기화
    - 무한 스크롤 리스너를 GridLayoutManager용으로 따로 만들어 주어야 함

--- app/src/main/java/com/joseph/unsplash_mvvm/ui/main/SearchViewModel.kt
+++ app/src/main/java/com/joseph/unsplash_mvvm/ui/main/SearchViewModel.kt
@@ -3,11 +3,13 @@ package com.joseph.unsplash_mvvm.ui.main
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.joseph.unsplash_mvvm.models.Photo
+import com.joseph.unsplash_mvvm.models.PhotoSearchResult
 import com.joseph.unsplash_mvvm.repo.PhotoRepository
 import com.joseph.unsplash_mvvm.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,20 +18,24 @@ class SearchViewModel @Inject constructor(
     private val photoRepository: PhotoRepository
 ) :ViewModel() {
 
-    sealed class SearchEvent() {
-        class SearchPhotosSuccess(data: List<Photo>) : SearchEvent()
-        class SearchPhotosFailed(message: String) : SearchEvent()
-        object SearchPhotosQueryEmpty : SearchEvent()
-        object Loading : SearchEvent()
-    }
-
-    private val _photos = MutableStateFlow<SearchEvent>(SearchEvent.SearchPhotosQueryEmpty)
-    val photos get() = _photos
+    private var _photos = MutableStateFlow<Resource<PhotoSearchResult>?>(null)
+    val photos: StateFlow<Resource<PhotoSearchResult>?> get() = _photos
 
     private var lastQuery = ""
     private var page = 1
+    var isSameQuery = false
 
-    fun searchPhotos(query: String) {
+    fun searchPhotos(query: String) = viewModelScope.launch {
+        _photos.value = Resource.Loading()
+        if(lastQuery == query) {
+            page++
+            isSameQuery = true
+            _photos.value = photoRepository.searchPhotos(query, page)
+        } else {
+            page = 1
+            isSameQuery = false
+            _photos.value = photoRepository.searchPhotos(query, page)
+        }
     }
 }
 
--- app/src/main/java/com/joseph/unsplash_mvvm/ui/main/fragments/SearchFragment.kt
+++ app/src/main/java/com/joseph/unsplash_mvvm/ui/main/fragments/SearchFragment.kt
@@ -5,17 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
 import com.joseph.unsplash_mvvm.R
+import com.joseph.unsplash_mvvm.adapters.PhotoAdapter
 import com.joseph.unsplash_mvvm.databinding.FragmentSearchBinding
 import com.joseph.unsplash_mvvm.ui.main.SearchViewModel
+import com.joseph.unsplash_mvvm.util.Resource
+import com.joseph.unsplash_mvvm.util.setupInfinityScrollListener
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 import timber.log.Timber
 
 @AndroidEntryPoint
 class SearchFragment : Fragment(R.layout.fragment_search) {
 
+    private var _query = ""
+    private val adapter = PhotoAdapter()
     private var _binding: FragmentSearchBinding? = null
     val binding get() = _binding!!
 
@@ -33,22 +43,53 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        collectSearchResult()
+        initializeRecyclerView()
+
         binding.imageSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                Timber.d("[TEXT] : ${query}")
+                _query = query ?: return false
+                viewModel.searchPhotos(_query)
                 return true
             }
 
-            override fun onQueryTextChange(newText: String?): Boolean {
-                Timber.d("[TEXT] : ${newText}")
-                return true
-            }
+            override fun onQueryTextChange(newText: String?): Boolean = false
         })
+    }
+
+    private fun initializeRecyclerView() = with(binding) {
+        val layoutManager = GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+        photoRecyclerview.adapter = adapter
+        photoRecyclerview.layoutManager = layoutManager
+        photoRecyclerview.setupInfinityScrollListener { viewModel.searchPhotos(_query) }
+    }
+
+    private fun collectSearchResult() = lifecycleScope.launchWhenStarted {
+        viewModel.photos.collect { state ->
+            when (state) {
+                is Resource.Success -> {
+                    binding.progressLottie.isVisible = false
+                    if (state.data?.results != null) {
+                        if(viewModel.isSameQuery) {
+                            adapter.submitList(adapter.currentList + state.data.results)
+                        } else {
+                            adapter.submitList(state.data.results)
+                        }
+                    }
+                }
+                is Resource.Loading -> {
+                    binding.progressLottie.isVisible = true
+                }
+                is Resource.Error -> {
+                    binding.progressLottie.isVisible = false
+                    Toast.makeText(requireContext(), state.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
-
 }
